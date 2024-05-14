edccf30ace9487f72a4d32f66d6a11fdc8ad4761
ahartfelder <ahartfelder@gmail.com>
2024-05-14 16:22:25 -0300

    Main Header

---
+++ components/main-header/main-header.js
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import logo from "@/assets/logo.png";
+import styles from "@/components/main-header/main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={styles.header}>
+        <Link className={styles.logo} href="/">
+          <Image src={logo} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/community">Community</Link>
+            </li>
+            <li>
+              <Link href="/meals">Meals</Link>
+            </li>
+            <li>
+              <Link href="/meals/share">Share Meal</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
